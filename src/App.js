31c1d68ef0f067e02f14b2c9ef729dd92d2d3a44
eyupucmaz <eyupucmaz@gmail.com>
2020-09-20 22:44:00 +0300

    customize with material ui

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { Button, FormControl, Input, InputLabel } from "@material-ui/core";
+import Todo from "./Todo";
+import db from "./firebase";
+import firebase from "firebase";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [todos, setTodos] = useState([]);
+	const [input, setInput] = useState("");
+
+	// ! When the app loads, we need to listen to the database and fetch new todos as they  get added/removed
+	useEffect(() => {
+		db.collection("todos")
+			.orderBy("timestamp", "desc")
+			.onSnapshot((snapshot) => {
+				setTodos(
+					snapshot.docs.map((doc) => ({
+						id: doc.id,
+						todo: doc.data().todo,
+					}))
+				);
+			});
+	}, []);
+
+	const addTodo = (e) => {
+		// this will fire off when click the button
+		e.preventDefault(); //it'll stop refresh the page
+
+		db.collection("todos").add({
+			todo: input,
+			//Adds input value to an object and sends database
+			timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+		});
+
+		setInput(""); //Clear the input state and input
+	};
+	return (
+		<div className="App">
+			<h1>🚀 Rocket To-Do !</h1>
+			<form>
+				<FormControl>
+					<InputLabel>📔 Type your Todo!</InputLabel>
+					<Input
+						value={input}
+						onChange={(event) => setInput(event.target.value)}
+					/>
+				</FormControl>
+				<Button
+					className="addbtn"
+					disabled={!input}
+					variant="contained"
+					color="primary"
+					type="submit"
+					onClick={addTodo}
+				>
+					Add Todo
+				</Button>
+			</form>
+
+			<ul className="todos">
+				{todos.map((todo) => (
+					<Todo todo={todo} />
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default App;
